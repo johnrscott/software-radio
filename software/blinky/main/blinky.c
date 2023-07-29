516637eb07dffefae21786e49fa63460f1cc1ae3
John Scott <johnrscott0@gmail.com>
2023-07-29 16:27:30 +0100

    Added DAC example, but found esp32-c3 has no DAC!

--- software/blinky/main/blinky.c
+++ software/blinky/main/blinky.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
     ESP_LOGI("main", "Hello from main");
     
-    while(true)
-	;
+    while(true) {
+	vTaskDelay(120 / portTICK_PERIOD_MS);
+    }
 }
